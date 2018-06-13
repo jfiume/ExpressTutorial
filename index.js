8888be20b0ed906c9dc53d443955186cf9b29677
Joe Fiume <joefiume88@gmail.com>
2018-06-13 15:25:56 -0700

    added a route to deliver all people

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ var personSchema = mongoose.Schema({
 });
 var Person = mongoose.model("Person", personSchema);
 
-app.get('/', function(req, res) {
-  res.render('form');
-});
-
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -30,6 +26,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // for parsing multipart/form-data
 app.use(upload.array());
 app.use(express.static('public'));
+
+app.get('/person', function(req, res) {
+  res.render('person');
+});
+
+app.post('/person', function(req, res){
+   var personInfo = req.body; //Get the parsed information
+   console.log(personInfo);
+
+   if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+      res.render('show_message', {
+         message: "Sorry, you provided worng info", type: "error"});
+   } else {
+      var newPerson = new Person({
+         name: personInfo.name,
+         age: personInfo.age,
+         nationality: personInfo.nationality
+      });
+
+      newPerson.save(function(err, Person){
+         if(err)
+            res.render('show_message', {message: "Database error", type: "error"});
+         else
+            res.render('show_message', {
+               message: "New person added", type: "success", person: personInfo});
+      });
+   }
+});
+
+app.get('/people', function(req, res) {
+  Person.find(function(err, response) {
+    res.json(response);
+  });
+});
+
+app.get('/', function(req, res) {
+  res.render('form');
+});
+
 
 app.post('/', function(req, res) {
   console.log(req.body);
