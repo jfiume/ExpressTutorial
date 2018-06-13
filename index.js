9757a4a4b01eeb54b5417d1b6478821913dfdd6f
Joe Fiume <joefiume88@gmail.com>
2018-06-12 19:34:21 -0700

    added forms with express

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var mutler = require('multer');
+var upload = mutler();
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_db');
+
+var personSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  nationality: String
+});
+var Person = mongoose.model("Person", personSchema);
 
 app.get('/', function(req, res) {
-  res.send("hello world!");
+  res.render('form');
 });
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+// form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array());
+app.use(express.static('public'));
+
+app.post('/', function(req, res) {
+  console.log(req.body);
+  res.send("recieved your request!")
+})
 
 app.listen(3000);
