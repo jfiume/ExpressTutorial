cc09899401dc042f259fa5f02586ce444552e4df
Joe Fiume <joefiume88@gmail.com>
2018-06-13 16:51:38 -0700

    working on RESTful routes

--- index.js
+++ index.js
@@ -1,74 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mutler = require('multer');
-var upload = mutler();
+var multer = require('multer');
+var cookieParser = require('cookie-parser');
+var upload = multer();
+
 var app = express();
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_db');
 
-var personSchema = mongoose.Schema({
-  name: String,
-  age: Number,
-  nationality: String
-});
-var Person = mongoose.model("Person", personSchema);
-
-app.set('view engine', 'pug');
-app.set('views', './views');
-
-// for parsing application/json
+app.use(cookieParser());
 app.use(bodyParser.json());
-
-// for parsing application/xwww-
 app.use(bodyParser.urlencoded({ extended: true }));
-// form-urlencoded
-
-// for parsing multipart/form-data
 app.use(upload.array());
-app.use(express.static('public'));
 
-app.get('/person', function(req, res) {
-  res.render('person');
-});
+//Require the Router we defined in movies.js
+var movies = require('./movies.js');
 
-app.post('/person', function(req, res){
-   var personInfo = req.body; //Get the parsed information
-   console.log(personInfo);
-
-   if(!personInfo.name || !personInfo.age || !personInfo.nationality){
-      res.render('show_message', {
-         message: "Sorry, you provided worng info", type: "error"});
-   } else {
-      var newPerson = new Person({
-         name: personInfo.name,
-         age: personInfo.age,
-         nationality: personInfo.nationality
-      });
-
-      newPerson.save(function(err, Person){
-         if(err)
-            res.render('show_message', {message: "Database error", type: "error"});
-         else
-            res.render('show_message', {
-               message: "New person added", type: "success", person: personInfo});
-      });
-   }
-});
-
-app.get('/people', function(req, res) {
-  Person.find(function(err, response) {
-    res.json(response);
-  });
-});
-
-app.get('/', function(req, res) {
-  res.render('form');
-});
-
-
-app.post('/', function(req, res) {
-  console.log(req.body);
-  res.send("recieved your request!")
-})
+//Use the Router on the sub route /movies
+app.use('/movies', movies);
 
 app.listen(3000);
---
+++ person.js
@@ -0,0 +1,81 @@
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var mutler = require('multer');
+// var upload = mutler();
+// var app = express();
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/my_db');
+//
+// var personSchema = mongoose.Schema({
+//   name: String,
+//   age: Number,
+//   nationality: String
+// });
+// var Person = mongoose.model("Person", personSchema);
+//
+// app.set('view engine', 'pug');
+// app.set('views', './views');
+//
+// // for parsing application/json
+// app.use(bodyParser.json());
+//
+// // for parsing application/xwww-
+// app.use(bodyParser.urlencoded({ extended: true }));
+// // form-urlencoded
+//
+// // for parsing multipart/form-data
+// app.use(upload.array());
+// app.use(express.static('public'));
+//
+// app.get('/person', function(req, res) {
+//   res.render('person');
+// });
+//
+// app.post('/person', function(req, res){
+//    var personInfo = req.body; //Get the parsed information
+//    console.log(personInfo);
+//
+//    if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+//       res.render('show_message', {
+//          message: "Sorry, you provided worng info", type: "error"});
+//    } else {
+//       var newPerson = new Person({
+//          name: personInfo.name,
+//          age: personInfo.age,
+//          nationality: personInfo.nationality
+//       });
+//
+//       newPerson.save(function(err, Person){
+//          if(err)
+//             res.render('show_message', {message: "Database error", type: "error"});
+//          else
+//             res.render('show_message', {
+//                message: "New person added", type: "success", person: personInfo});
+//       });
+//    }
+// });
+//
+// app.get('/people', function(req, res) {
+//   Person.find(function(err, response) {
+//     res.json(response);
+//   });
+// });
+//
+// app.put('/people/:id', function(req, res) {
+//   Person.findByIdAndUpdate(req.params.id, req.body, function(err, response){
+//     if(err) res.json({message: "Error in updating person with id" + req.params.id});
+//     res.json(response);
+//   });
+// });
+//
+// app.get('/', function(req, res) {
+//   res.render('form');
+// });
+//
+//
+// app.post('/', function(req, res) {
+//   console.log(req.body);
+//   res.send("recieved your request!")
+// })
+//
+// app.listen(3000);
