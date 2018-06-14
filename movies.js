b4c244719659c0412a17625e7520a4488bf3e101
Joe Fiume <joefiume88@gmail.com>
2018-06-13 17:47:23 -0700

    fully explored RESTful apis in Express/Node

--- movies.js
+++ movies.js
@@ -26,4 +26,73 @@ router.get('/:id([0-9]{3,})', function(req, res) {
   }
 });
 
+router.post('/', function(req, res) {
+  //Check if all fields are provided and are valid:
+  if(!req.body.name ||
+      !req.body.year.toString().match(/^[0-9]{4}$/g) ||
+      !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g)
+  ){
+    res.status(400);
+    res.json({message: "Bad Request"});
+  } else {
+    var newId = movies[movies.length-1].id+1;
+    movies.push({
+      id: newId,
+      name: req.body.name,
+      year: req.body.year,
+      rating: req.body.rating
+    });
+    res.json({message: "New movie created.", location: "/movies/" + newId});
+  }
+});
+
+router.put('/:id', function(req, res) {
+  if(!req.body.name ||
+      !req.body.year.toString().match(/^[0-9]{4}$/g) ||
+      !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g) ||
+      !req.params.id.toString().match(/^[0-9]{3,}$/g)){
+
+      res.status(400);
+      res.json({message: "Bad Request"});
+    } else {
+      //Gets us the index of the movie with the given id
+      var updateIndex = movies.map(function(movie){
+        return movie.id;
+      }).indexOf(parseInt(req.params.id));
+
+      if(updateIndex === -1) {
+        //Movie not found, create new
+        movies.push({
+          id: req.params.id,
+          name: req.body.name,
+          year: req.body.year,
+          rating: req.body.rating
+        });
+        res.json({message: "New movie created.", location: "/movies/" + req.params.id});
+      } else {
+        //Update existing movie
+        movies[updateIndex] = {
+          id: req.params.id,
+          name: req.body.name,
+          year: req.body.year,
+          rating: req.body.rating
+        };
+        res.json({message: "Movie id " + req.params.id + " updated.", location: "/movies/" + req.params.id});
+      }
+    }
+});
+
+router.delete('/:id', function(req, res){
+  var removeIndex = movies.map(function(movie){
+    return movie.id;
+  }).indexOf(parseInt(req.params.id)); //Get us the index of the movie with the given id.
+
+  if(removeIndex === -1){
+    res.json({message: "Not Found"});
+  } else {
+    movies.splice(removeIndex, 1);
+    res.send({message: "Movie id  " + req.params.id + " removed"});
+  }
+});
+
 module.exports = router;
